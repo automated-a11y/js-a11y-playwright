ab07a0b56c12f6a7f585c2055474a83c66369019
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2023-01-31 22:51:17 +0000

    Tests

---
+++ tests/axetest.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import AxeRunner from "../src/axerunner.js";
+
+let axerunner;
+
+test.beforeEach(async ({ page }, testInfo) => {
+    axerunner = new AxeRunner(page)
+});
+
+test('axe test', async ({ page }) => {
+    await page.goto(`file://${process.env.PWD}/tests/test.html`);
+    let data = await axerunner.setPageTitle('Page Title').execute()
+    expect(data.violations.length).toEqual(2)
+});
---
+++ tests/htmlcstest.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import HtmlCsRunner from "../src/htmlcsrunner.js";
+
+let htmlcsrunner;
+
+test.beforeEach(async ({ page }, testInfo) => {
+    htmlcsrunner = new HtmlCsRunner(page)
+});
+
+test('htmlcs test', async ({ page }) => {
+    await page.goto(`file://${process.env.PWD}/tests/test.html`);
+    let data = await htmlcsrunner.setPageTitle('Page Title').execute()
+    expect(data.errors).toEqual(5)
+});
